e3ae8dd75a1cf2ca1001f55a4bf684b383fe28f9
Kelly Kong <kellykong2772@gmail.com>
2017-12-19 17:20:56 +0800

    fix header 1 bug

--- resources/views/layouts/app_layout.blade.php
+++ resources/views/layouts/app_layout.blade.php
@@ -5,71 +5,12 @@
 @endsection
 
 @section('body')
-{{-- <div id="app">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    &nbsp;
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right nav-stacked">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ route('login') }}">Login</a></li>
-                        <li><a href="{{ route('register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li>
-                                    <a href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    @yield('content')
-</div> --}}
 <div class="wrapper">
 
    <nav id="sidebar">
       <!-- Sidebar Header -->
       <div class="sidebar-header">
-         <h3>Collapsible Sidebar</h3>
+         <div class="sidebar-bg"></div>
       </div>
 
       <!-- Sidebar Links -->
@@ -89,7 +30,6 @@
          <li><a href="#">Contact</a></li>
       </ul>
    </nav>
-    
    <div id="content">
       <div class="content-heading container-fluid">
          <div class="row">
@@ -98,7 +38,7 @@
                    <i class="glyphicon glyphicon-menu-hamburger"></i>
                </button>
             </div>
-            <div class="col-sm-2">
+            <div class="col-sm-2 hidden-sm hidden-xs ">
                <div class="desktop_profile nav">
                   @if (Auth::guest())
                      <li><a href="{{ route('login') }}">Login</a></li>
